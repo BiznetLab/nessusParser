b6116183eef4c1257ed54aa78aa0d3056f5384af
behruzcebiyev <behruzcebiyev@gmail.com>
2017-03-17 16:57:33 +0200

    Update nessus.sh

--- nessus.sh
+++ nessus.sh
@@ -4,7 +4,7 @@ printf "\t";echo \"$1_order.txt\" created.
 
 grep -r '^\"[0-9][0-9][0-9][0-9].*' $1 | awk -F"," '{ print $1,$5,$7 }' > $1_res.txt;
 
-grep "^\"11936\|Remote operating system :" tmp.csv | cut -d"," -f5 | sed 's/^.*: //g' | tr -d "\""  > os_identified.txt 
+grep "^\"11936\|Remote operating system :" $1 | cut -d"," -f5 | sed 's/^.*: //g' | tr -d "\""  > os_identified.txt 
 
 sed 's/" "/,/g' $1_res.txt | sed 's/"//g' | uniq | sort -g > $1_result.txt;
 
